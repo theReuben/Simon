64508c7b71138ae655403a1d857eb432d3c3037a
Reuben Day <reubenday@btinternet.com>
2020-04-01 23:48:23 +0100

    Further additions, possibly ready to merge

--- custom.py
+++ custom.py
@@ -1,26 +1,24 @@
 from libs.attack_pattern import AttackPattern
-from libs.functions import accept_drop
+from libs.functions import accept_drop, turn_in
 
 from datetime import datetime as dt
 from time import sleep
 from sys import argv
 
-def new_lr():
-	return AttackPattern([(2,1.5), (3,1.5), (4,1.5), (0,0),	(5,1.5)])
-
+def tainted_gems():
+	while True:
+		turn_in(num=7)
 
 if __name__ == '__main__':
-	lr = new_lr()
-	accept = False
+	var = argv[1]
 	func = False
-	num = 0
-	args = len(argv) - 1
-	var = argv[1][1::]
-	if args >= 2 and argv[2][1::] == "a":
-		accept = True
 
-	if args == 1:
-		func = lr
+	if var == "tg":
+		func = tainted_gems
+	elif var == "test" :
+		sleep(3)
+		# Your code here
+		print("Test complete.")
 
 	if func != False:
 		print("Running from: {0}".format(dt.now()))
